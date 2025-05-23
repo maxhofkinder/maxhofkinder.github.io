bdd23849d0d4f0ad2ec74c7b9519c3ba093d0656
madmin25 <the-evil@gmx.de>
2025-05-23 12:38:03 +0200

    github pages workflow added

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/maxhofkinder' : '',
+  images: {
+    unoptimized: true,
+  },
   // Other config options can remain unchanged
 };
 
 export default nextConfig;
+
 
